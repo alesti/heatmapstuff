adfbb400895b2f7e901fb20c4093d7bbffe82824
Alexander Stielau <alexander.stielau@googlemail.com>
2021-12-04 19:59:02 +0100

    removed location data

--- bin/load-trackfile.sh
+++ bin/load-trackfile.sh
@@ -3,35 +3,31 @@
 
 # maximal allowed distance in m (15km)
 maxdist=15000
-# logfile
 logfile=/tmp/trackmunging.log
+
+# logfile
 rm -f $logfile
 touch $logfile
-# Array schmutz
-IFS=$'\n'
-# while read line; do array+=($(echo $line | grep '<trkpt'| sed -e 's/<trkpt lat="//' -e 's/" lon="/:/' -e 's/">//')) ; done < testfile_short.gpx
 
-# delete last array
-unset coords
+
 echo "Importing trackpoints from $1 - this needs some time"
+unset coords
 # example line
 #      <trkpt lat="54.757862600" lon="11.461799800">
+IFS=$'\n'
 while read line  
 do 
   coords+=($(echo $line | grep '<trkpt'| sed -e 's/.*<trkpt lat="//' -e 's/" lon="/ /' -e 's/">//')) 
 done < $1 
 
-#for value in "${coords[@]}" ; do echo $value >> $logfile ; done
 # search for dupes
-echo "Checkin dupes:" >> $logfile
-printf '%s\n' "${coords[@]}"|awk '!($0 in seen){seen[$0];next} 1' >> $logfile
-
-# Werte rauspopeln und an distance checker geben.
+#echo "Checkin dupes:" >> $logfile
+#printf '%s\n' "${coords[@]}"|awk '!($0 in seen){seen[$0];next} 1' >> $logfile
 
 elements=${#coords[*]}
 echo "We have $elements coordinate points in this file"
 
-# check if uneven, if discard last element
+# check if uneven, discard last element
 if [[ $((elements % 2)) -eq 0 ]]; then
   :
 else
@@ -54,14 +50,14 @@ do
  #echo -e "$n \t$lat1 \t$lon1" >> $logfile
  #echo -e "$m \t$lat2 \t$lon2" >> $logfile
 
- # haversine does the distance compute stuff :-)
+ # haversine does the distance compute stuff
  distance=$(./haversine.sh $lat1 $lon1 $lat2 $lon2)
 
  if [[ $distance -gt $maxdist ]] ; then
    echo "Distance between Point $n to $m is $distance meters, check in ge: \"from: ${coords[$n]} to: ${coords[$m]}\""
    # find ${coords[$m]} in $1, delete the complete entry
    echo "Found $(grep -c "$lat2\" lon=\"$lon2" $1) occurrencies"
-   sed -i "/$lat2\" lon=\"$lon2/,+3d" $1 # deletes all occurrencies, i want only to kill the first one...
+   sed -i "/$lat2\" lon=\"$lon2/,+3d" $1 # deletes all occurrencies!
    echo "Deleted a set of wrong trackpoints, run me again :-)"
    break
  fi  
